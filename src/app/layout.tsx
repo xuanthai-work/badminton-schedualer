5e9f7abf90c42a0179f3caae1c68460bcdafec53
jayz <jayz.bui@techvify.com.vn>
2026-06-08 13:40:31 +0700

    chore: add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { I18nProvider } from "@/lib/i18n";
 import UpdatePrompt from "@/components/UpdatePrompt";
 
@@ -51,6 +52,7 @@ export default function RootLayout({
           {children}
           <UpdatePrompt />
         </I18nProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
